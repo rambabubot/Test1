026bc69be140431c015c39f9e8bf439e5ded7f00
JayR1213 <111758139+JayR1213@users.noreply.github.com>
2023-06-01 18:21:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -70,11 +70,11 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b> अगर आप मूवी या वेब सीरीज लेना चाहते हैं तो यहां पर क्लिक करें 👇🏻 ( PLEASE SEARCH HERE )
-</br>
-https://t.me/+K7G6vLz76Ws4MzJl   </br>  
-https://t.me/+K7G6vLz76Ws4MzJl   </br>
-https://t.me/+K7G6vLz76Ws4MzJl   </br> </b>")
+    await message.reply_text("<b> अगर आप मूवी या वेब सीरीज लेना चाहते हैं तो यहां पर क्लिक करें 👇🏻 </b>")
+    btn.insert(0, [
+        InlineKeyboardButton('PLEASE SEARCH HERE 🔍', url="https://t.me/+K7G6vLz76Ws4MzJl")
+    ])
+
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
